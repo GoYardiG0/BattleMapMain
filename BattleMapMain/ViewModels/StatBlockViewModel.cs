d4769275b49020e560ef917332df82d6fcb802ba
User <User@LAB3-3>
2024-12-03 12:51:15 +0200

    *unfuck you

--- BattleMapMain/ViewModels/StatBlockViewModel.cs
+++ BattleMapMain/ViewModels/StatBlockViewModel.cs
@@ -51,7 +51,7 @@ namespace BattleMapMain.ViewModels
         }
 
         #endregion
-         //cooment for pushin
+         //cooment for pushin //again
         #region ac
 
         private int ac;
