b698f458fb1c0ceee6c9d72bceebc9388054b704
Yarden Golan <yarden.golan07@gmail.com>
2025-06-03 19:26:50 +0300

    fixed miniן

--- BattleMapMain/Classes and Objects/Mini.cs
+++ BattleMapMain/Classes and Objects/Mini.cs
@@ -28,27 +28,10 @@ namespace BattleMapMain.Classes_and_Objects
 
         public int Ac { get; set; }
 
-        public int Hp { get; set; }
-
-        public int Str { get; set; }
-
-        public int Dex { get; set; }
-
-        public int Con { get; set; }
-
-        public int Int { get; set; }
-
-        public int Wis { get; set; }
-
-        public int Cha { get; set; }
+        public int Hp { get; set; }        
 
         public int Level { get; set; }
-
-        public string? PassiveDesc { get; set; }
-
-        public string? ActionDesc { get; set; }
-
-        public string? SpecialActionDesc { get; set; }
+        
         public Mini() { }
         public Mini(Monster monster)
         {
@@ -64,17 +47,8 @@ namespace BattleMapMain.Classes_and_Objects
             this.monster = monster;
             this.ImgURL = monster.MonsterPicURL;
             this.Ac = monster.Ac;
-            this.Hp = monster.Hp;
-            this.Str = monster.Str;
-            this.Dex = monster.Dex;
-            this.Con = monster.Con;
-            this.Int = monster.Int;
-            this.Wis = monster.Wis;
-            this.Cha = monster.Cha;
-            this.Level = monster.Cr;
-            this.PassiveDesc = monster.PassiveDesc;
-            this.ActionDesc = monster.ActionDesc;
-            this.SpecialActionDesc = monster.SpecialActionDesc;
+            this.Hp = monster.Hp;          
+            this.Level = monster.Cr;           
         }
         public Mini(Character character)
         {
@@ -91,10 +65,7 @@ namespace BattleMapMain.Classes_and_Objects
             this.ImgURL = character.CharacterPicURL;
             this.Ac = character.Ac;
             this.Hp = character.Hp;        
-            this.Level = character.Level;
-            this.PassiveDesc = character.PassiveDesc;
-            this.ActionDesc = character.ActionDesc;
-            this.SpecialActionDesc = character.SpecialActionDesc;
+            this.Level = character.Level;           
         }
         public Mini(Mini mini)
         {
@@ -111,20 +82,10 @@ namespace BattleMapMain.Classes_and_Objects
                 if (count > 0)
                     this.Name += $"_{count}";
                 this.monster = monster;
-                currentHP = monster.Hp;
                 this.ImgURL = monster.MonsterPicURL;
                 this.Ac = monster.Ac;
                 this.Hp = monster.Hp;
-                this.Str = monster.Str;
-                this.Dex = monster.Dex;
-                this.Con = monster.Con;
-                this.Int = monster.Int;
-                this.Wis = monster.Wis;
-                this.Cha = monster.Cha;
                 this.Level = monster.Cr;
-                this.PassiveDesc = monster.PassiveDesc;
-                this.ActionDesc = monster.ActionDesc;
-                this.SpecialActionDesc = monster.SpecialActionDesc;
             }
             else if (mini.character != null)
             {
@@ -139,20 +100,10 @@ namespace BattleMapMain.Classes_and_Objects
                 if (count > 0)
                     this.Name += $"_{count}";
                 this.character = character;
-                this.currentHP = character.Hp;
                 this.ImgURL = character.CharacterPicURL;
                 this.Ac = character.Ac;
                 this.Hp = character.Hp;
-                this.Str = character.Str;
-                this.Dex = character.Dex;
-                this.Con = character.Con;
-                this.Int = character.Int;
-                this.Wis = character.Wis;
-                this.Cha = character.Cha;
                 this.Level = character.Level;
-                this.PassiveDesc = character.PassiveDesc;
-                this.ActionDesc = character.ActionDesc;
-                this.SpecialActionDesc = character.SpecialActionDesc;
             }
             if (mini.img != null)
                 this.img = mini.img;
